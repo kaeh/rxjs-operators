2d5e4ea11fbc4b98b91edefa20b4e74656d4cb34
Corentin GROSDEMOUGE <corentin.grosdemouge@gmail.com>
2021-09-07 23:08:02 +0200

    handle predicate on switch-map-if

--- src/switch-map-if/switch-map-if.spec.ts
+++ src/switch-map-if/switch-map-if.spec.ts
@@ -2,23 +2,59 @@ import { of } from "rxjs";
 import { switchMapIf } from "./switch-map-if";
 
 describe(switchMapIf.name, () => {
-  it("should switch to observable when condition is met", (done) => {
-    const source1$ = of(1);
-    const source2$ = of(2);
+  describe("With observable", () => {
+    it("should switch to observable when condition is met", (done) => {
+      const source1$ = of(1);
+      const source2$ = of(2);
 
-    source1$.pipe(switchMapIf((x) => x === 1, source2$)).subscribe((result) => {
-      expect(result).toBe(2);
-      done();
+      source1$.pipe(switchMapIf((x) => x === 1, source2$)).subscribe((result) => {
+        expect(result).toBe(2);
+        done();
+      });
+    });
+
+    it("should not switch to observable when condition is met", (done) => {
+      const source1$ = of(1);
+      const source2$ = of(2);
+
+      source1$.pipe(switchMapIf((x) => x === 42, source2$)).subscribe((result) => {
+        expect(result).toBe(1);
+        done();
+      });
     });
   });
 
-  it("should not switch to observable when condition is met", (done) => {
-    const source1$ = of(1);
-    const source2$ = of(2);
+  describe("With predicate", () => {
+    it("should switch to observable when condition is met", (done) => {
+      const source1$ = of(1);
 
-    source1$.pipe(switchMapIf((x) => x === 42, source2$)).subscribe((result) => {
-      expect(result).toBe(1);
-      done();
+      source1$
+        .pipe(
+          switchMapIf(
+            (x) => x === 1,
+            () => of(2)
+          )
+        )
+        .subscribe((result) => {
+          expect(result).toBe(2);
+          done();
+        });
+    });
+
+    it("should not switch to observable when condition is met", (done) => {
+      const source1$ = of(1);
+
+      source1$
+        .pipe(
+          switchMapIf(
+            (x) => x === 42,
+            () => of(2)
+          )
+        )
+        .subscribe((result) => {
+          expect(result).toBe(1);
+          done();
+        });
     });
   });
 });
--- src/switch-map-if/switch-map-if.ts
+++ src/switch-map-if/switch-map-if.ts
@@ -1,5 +1,13 @@
 import { Observable, of, OperatorFunction } from "rxjs";
 import { switchMap } from "rxjs/operators";
 
-export const switchMapIf = <T = unknown>(condition: (x: T) => boolean, obs: Observable<T>): OperatorFunction<T, T> =>
-  switchMap((value) => (condition(value) ? obs : of(value)));
+export function switchMapIf<T = unknown>(condition: (x: T) => boolean, obs: Observable<T>): OperatorFunction<T, T>;
+export function switchMapIf<T = unknown>(condition: (x: T) => boolean, predicate: (x: T) => Observable<T>): OperatorFunction<T, T>;
+export function switchMapIf<T = unknown>(
+  condition: (x: T) => boolean,
+  obsOrPredicate: Observable<T> | ((x: T) => Observable<T>)
+): OperatorFunction<T, T> {
+  return switchMap((value) =>
+    condition(value) ? (typeof obsOrPredicate === "function" ? obsOrPredicate(value) : obsOrPredicate) : of(value)
+  );
+}
