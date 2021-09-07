79378de98307ecf4f8cdf7d64a31b50b731edef3
Corentin GROSDEMOUGE <corentin.grosdemouge@gmail.com>
2021-09-07 22:46:24 +0200

    fix types errors

--- src/execute-on-error/execute-on-error.ts
+++ src/execute-on-error/execute-on-error.ts
@@ -3,7 +3,7 @@ import { catchError } from "rxjs/operators";
 
 interface Options<T = unknown> {
   suppressErrors: boolean;
-  defaultValue: T;
+  defaultValue: T | null | undefined;
 }
 
 const defaultOptions: Options<null> = { suppressErrors: false, defaultValue: null };
@@ -13,10 +13,13 @@ const defaultOptions: Options<null> = { suppressErrors: false, defaultValue: nul
  *
  * Options gives the possibility to suppress the error and return a default value in the stream instead
  */
-export const executeOnError = <T = unknown>(fn: (error: unknown) => void, options?: Partial<Options<T>>): OperatorFunction<T, T> => {
+export const executeOnError = <T = unknown>(
+  fn: (error: unknown) => void,
+  options?: Partial<Options<T>>
+): OperatorFunction<T, T | null | undefined> => {
   const sanitizedOptions = { ...defaultOptions, ...(options ? options : {}) };
 
-  return catchError<T, Observable<T>>((err) => {
+  return catchError<T, Observable<T | null | undefined>>((err: unknown) => {
     fn(err);
     return sanitizedOptions.suppressErrors ? of(sanitizedOptions.defaultValue) : throwError(err);
   });
